cd7244672a817360b1838c8bfd8822b4b50d275d
hrmrakib <hrmrakibs@gmail.com>
2024-08-02 11:14:54 +0600

    solve highAndLow number from string numbers

--- easy/string/highAndLow.js
+++ easy/string/highAndLow.js
@@ -1,3 +1,15 @@
-function highAndLow(p) {}
+// execute highest and lowest number from string
+
+function highAndLow(p) {
+  const strArr = p.split(" ");
+  const numberArr = [];
+  for (let i of strArr) {
+    numberArr.push(parseInt(i));
+  }
+  return `${Math.min(...numberArr)} ${Math.max(...numberArr)}`;
+}
+
+// Different Way
+function highAndLow2(p) {}
 
 console.log(highAndLow("11, 21, 46, 98, -9, 34, -2, 50"));
