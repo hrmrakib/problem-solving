37b889fd8dd6b12ef915b6dcff15ac63e7102c51
hrmrakib <hrmrakibs@gmail.com>
2024-12-24 15:42:03 +0600

    sum index of an array

--- easy/array/sumIndexOfArray.js
+++ easy/array/sumIndexOfArray.js
@@ -1,1 +1,17 @@
-const arr = [13, 22, 43, 95, 73, 67, 56, 54, 37, 75, 37];
+/**
+ * Sum only indexes of an array, not element
+ * 0, 1, 2, 3, 4, 5, .......... (Totoal index of an Array)
+ */
+
+const arr = [13, 22, 23, 23, 32, 43, 95, 73, 67, 56, 54, 37, 75, 37];
+
+const sumIndexOfArray = (arr) => {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+console.log(sumIndexOfArray(arr));
